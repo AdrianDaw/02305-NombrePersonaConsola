c1be301dc3953c0fb1a457ed93a4ffa12d2af79e
AdrianDaw <adrianp899.estudios@gmail.com>
2019-10-30 18:54:59 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright 2019 Adrián Pérez Rodríguez - adrianp899@gmail.com .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrián Pérez Rodríguez - adrianp899@gmail.com
+ */
+public class Main {
+
+    
+    //Instanciar Scanner
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+    
+    public static void main(String[] args) {
+// Variable
+        String nombre;
+
+        // Leer dato
+        System.out.print("Me llamo ...: ");
+        nombre = SCN.nextLine();
+
+        // Mensaje
+        System.out.printf("Me llamo ..32: %s%n", nombre);
+    }
+}
